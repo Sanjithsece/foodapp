150d80ed697e6ea0e1eaa15d7880710d8e39cb9c
sanjithcce <sanjith.s2023cce@sece.ac.in>
2025-01-30 21:24:35 +0530

    Your commit message here

--- foodapp.js
+++ foodapp.js
@@ -162,7 +162,7 @@ app.post("/api/orders", authenticate, async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT ;
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
